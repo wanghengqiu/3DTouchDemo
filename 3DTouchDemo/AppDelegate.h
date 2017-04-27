2667024f4757347f57235640290d84e0936acbc0
wanghengqiu <wanghenqiu@163.com>
2017-04-28 00:35:01 +0800

    Initial Commit

---
+++ 3DTouchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  3DTouchDemo
+//
+//  Created by 王恒求 on 2017/4/28.
+//  Copyright © 2017年 王恒求. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
